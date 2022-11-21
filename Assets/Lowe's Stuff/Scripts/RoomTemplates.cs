2bef4b08e6663bf8fb2d8c111bde0a02ef9235ce
LoweGoth <lowe.goth@elev.ga.lbs.se>
2022-11-21 10:02:17 +0100

    Prefabs
    Dungeon generator

---
+++ Assets/Lowe's Stuff/Scripts/RoomTemplates.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public GameObject[] bottomRooms;
+    public GameObject[] topRooms;
+    public GameObject[] leftRooms;
+    public GameObject[] rightRooms;
+    public GameObject[] EndRooms;
+    public GameObject[] furniture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
